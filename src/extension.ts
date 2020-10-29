05971d971045eb57cc754da506f12aa5c47bac36
Gareth Brown <g@appsoftware.com>
2020-10-29 16:02:20 +0000

    Strip text in selection

--- src/extension.ts
+++ src/extension.ts
@@ -13,14 +13,63 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('no-control.helloWorld', () => {
+	let disposable1 = vscode.commands.registerCommand('no-control.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from No Control!');
 	});
 
-	context.subscriptions.push(disposable);
+	let disposable2 = vscode.commands.registerCommand('no-control.removeControlCharacters', () => {
+		// The code you place here will be executed every time your command is executed
+
+		const editor = vscode.window.activeTextEditor
+
+		// Reference for non printable characters: https://www.regular-expressions.info/nonprint.html
+
+		const controlCharsRegex = /[^\x00-\x7f]/gm;
+
+		if(editor)
+		{
+			const selection = editor.selection
+
+			if(selection)
+			{
+				let text = editor.document.getText(selection)
+
+				if(text)
+				{
+					if(text.match(controlCharsRegex))
+					{
+						// Control characters found, replace and confirm to user
+
+						text = text.replace(controlCharsRegex, 'C');
+
+						vscode.window.showInformationMessage('Control characters removed.');
+
+						editor.edit(builder => builder.replace(selection, text))
+					}
+					else
+					{
+						// No control characters found
+
+						vscode.window.showInformationMessage('No control characters found to remove.');
+					}
+				}
+				else
+				{
+					vscode.window.showInformationMessage('No text. Highlight the text for replacement of control characters.');
+				}
+			}
+			else
+			{
+				vscode.window.showInformationMessage('No selection. Highlight the text for replacement of control characters.');
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable1);
+	context.subscriptions.push(disposable2);
 }
 
 // this method is called when your extension is deactivated
